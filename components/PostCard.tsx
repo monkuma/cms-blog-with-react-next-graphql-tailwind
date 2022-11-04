911e95f9907370b391da8aa6a12384e06fc8b87b
monkuma <kumamoto.jobs@gmail.com>
2022-11-04 17:27:14 +0900

    add Link to PostCard Image

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -7,11 +7,13 @@ const PostCard: React.FC<PostRap> = ({ post }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
       <div className="relative overflow-hidden shadow-md pb-80 mb-6">
-        <img
-          src={post.featuredImage.url}
-          alt={post.title}
-          className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
-        />
+        <Link href={`/post/${post.slug}`}>
+          <img
+            src={post.featuredImage.url}
+            alt={post.title}
+            className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
+          />
+        </Link>
       </div>
       <h1 className="transition duration-70 text-center mb-8 cursor-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold ">
         <Link href={`/post/${post.slug}`}>{post.title}</Link>
