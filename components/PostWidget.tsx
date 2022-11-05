00b422d977a548ee991299feec836fc7b694eb3d
monkuma <kumamoto.jobs@gmail.com>
2022-11-05 20:16:22 +0900

    fix PostWidget

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { getRecentPosts, getSimilarPosts } from "../services/index";
 import moment from "moment";
 import Link from "next/link";
