935858e89eef9c8fd826e23dc21389938cdad3a9
monkuma <kumamoto.jobs@gmail.com>
2022-11-06 09:23:03 +0900

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
-import React, { useState, useEffect } from 'react'
-import { Layout } from '../components'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import React from "react";
+import { Layout } from "../components";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
